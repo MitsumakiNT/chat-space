1eeb67f309ee3f9f6d72b6769b5b36322d672536
mitsumakiNT <mitumakifr@gmail.com>
2020-03-10 15:55:28 +0900

    sending-message

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :update, :edit, :index]
+  resources :groups, only: [:new, :create, :update, :edit] do
+    resources :messages, only: [:index, :create]
+  end
+
 end
