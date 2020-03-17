685c8a4fbcf6856729f19d36c75343e37aff5674
mitsumakiNT <mitumakifr@gmail.com>
2020-03-17 19:49:25 +0900

    resetting cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '18.178.97.62', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -60,4 +61,3 @@
 #     # password: "please use keys"
 #   }
 
-server '18.178.97.62', user: 'ec2-user', roles: %w{app db web}
